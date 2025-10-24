db4d61077df8ca07adfe6f6bfa2e05cf1f9cc5d3
SrCastilloo <daniiterador@gmail.com>
2025-10-24 09:17:02 +0200

    Primera versión: proyecto Vaadin + Spring Boot + MySQL funcionando

---
+++ src/main/java/es/uca/orderflow/ui/MainView.java
@@ -0,0 +1,13 @@
+package es.uca.orderflow.ui;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("") // http://localhost:8080
+public class MainView extends VerticalLayout {
+  public MainView() {
+    add(new H1("Front-Office listo con Vaadin + Spring + MySQL 🚀"));
+    setSizeFull();
+  }
+}
