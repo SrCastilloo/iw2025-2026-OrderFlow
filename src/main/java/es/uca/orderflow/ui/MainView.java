25adaaee19d321228ea800972234ce45ea5f2496
SrCastilloo <daniiterador@gmail.com>
2025-10-24 09:22:54 +0200

    Pequeños cambios

--- src/main/java/es/uca/orderflow/ui/MainView.java
+++ src/main/java/es/uca/orderflow/ui/MainView.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.router.Route;
 @Route("") // http://localhost:8080
 public class MainView extends VerticalLayout {
   public MainView() {
-    add(new H1("Front-Office listo con Vaadin + Spring + MySQL 🚀"));
+    add(new H1("Front-Office listo con Vaadin + Spring + MySQL "));
     setSizeFull();
   }
 }
